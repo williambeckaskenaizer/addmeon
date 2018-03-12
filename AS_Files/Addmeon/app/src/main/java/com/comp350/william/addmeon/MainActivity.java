d4edb141f0d328ef9a9ba2eddbc46c595efad815
Specularity <willionaire14@gmail.com>
2018-03-12 12:10:56 -0700

    what

--- AS_Files/Addmeon/app/src/main/java/com/comp350/william/addmeon/MainActivity.java
+++ AS_Files/Addmeon/app/src/main/java/com/comp350/william/addmeon/MainActivity.java
@@ -1,13 +1,41 @@
 package com.comp350.william.addmeon;
 
+
+
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
+
+
 
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        // Capture the layout's TextView and set the string as its text
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(message);
     }
+
+    public void buttonOnClick(View view){
+        Button button = (Button) view;
+        ((Button) view).setText("clicked");
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+
+    }
+
 }
