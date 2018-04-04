7758f866b767f16459ca15bc2099afa59d58bfef
MatthewB29 <matthew.burdick913@myci.csuci.edu>
2018-04-04 01:50:04 -0700

    Steam Login Code
    Plan to properly implement it when I have the time, but hopefully we can use this

--- AS_Files/Addmeon/app/src/main/java/com/comp350/william/addmeon/AccountManager.java
+++ AS_Files/Addmeon/app/src/main/java/com/comp350/william/addmeon/AccountManager.java
@@ -48,6 +48,62 @@ public class AccountManager extends Activity implements AdapterView.OnItemSelect
 
         if(pos == 0){
             //steam
+
+            /*
+            public class LoginActivity extends ActionBarActivity {
+
+                // The string will appear to the user in the login screen
+                // you can put your app's name
+                final String REALM_PARAM = "YourAppName";
+
+                @Override
+                protected void onCreate(Bundle savedInstanceState) {
+                    super.onCreate(savedInstanceState);
+
+                    final WebView webView = new WebView(this);
+                    webView.getSettings().setJavaScriptEnabled(true);
+
+                    final Activity activity = this;
+
+                    webView.setWebViewClient(new WebViewClient()
+                    {
+                        @Override
+
+                        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+
+                         //checks the url being loaded
+                            setTitle(url);
+                            Uri Url = Uri.parse(url);
+
+                            if(Url.getAuthority().equals(REALM_PARAM.toLowerCase()))
+                            {
+                                // That means that authentication is finished and the url contains user's id.
+                                webView.stopLoading();
+
+                                // Extracts user id.
+                                Uri userAccountUrl = Uri.parse(Url.getQueryParameter("openid.identity"));
+                                String userId = userAccountUrl.getLastPathSegment();
+
+                                // Do whatever you want with the user's steam id
+
+                            });
+                            setContentView(webView);
+
+                            // Constructing openid url request
+                            String url = "https://steamcommunity.com/openid/login?" +
+                                    "openid.claimed_id=http://specs.openid.net/auth/2.0/identifier_select&" +
+                                    "openid.identity=http://specs.openid.net/auth/2.0/identifier_select&" +
+                                    "openid.mode=checkid_setup&" +
+                                    "openid.ns=http://specs.openid.net/auth/2.0&" +
+                                    "openid.realm=https://" + REALM_PARAM + "&" +
+                                    "openid.return_to=https://" + REALM_PARAM + "/signin/";
+
+                            webView.loadUrl(url);
+
+                        }
+                    }
+            */
+            
             listAdapter.add("Steam");
         }else if(pos == 1){
             //XBL
